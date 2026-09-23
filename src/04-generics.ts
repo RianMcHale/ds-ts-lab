288a770013486f09e84d95a1d052e253ed228aff
Rian McHale <rianmchwork@gmail.com>
2026-09-23 10:42:26 +0100

    Functions with generics

---
+++ src/04-generics.ts
@@ -0,0 +1,27 @@
+import {friends, colleagues} from './01-basics'
+import {Friend, Colleague} from './mytypes'
+
+function findMatch<T>( data : T[], criteria: (d: T) => boolean ) : T | undefined {
+    return data.find((criteria))
+}
+
+console.log(findMatch<Friend>(friends, (f) => f.name.startsWith('Jane')  ))
+console.log(findMatch<Colleague>(colleagues.current, (c) => c.department === 'Finance'  ))
+
+function sort<T>(
+  data: T[],
+  compare: (a: T, b: T) => number
+): T[] {
+  const copy = [...data];
+  return copy.sort(compare);
+}
+
+// Sort friends by age
+console.log(sort<Friend>(friends, (a, b) => a.age - b.age));
+// Sort colleagues by extension number
+console.log(
+  sort<Colleague>(
+    colleagues.current,
+    (a, b) => a.contact.extension - b.contact.extension
+  )
+);
